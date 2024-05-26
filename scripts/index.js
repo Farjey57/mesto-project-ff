ae194bfbb6674295cd512b0a7af6e78c4619018b
Sergey Tsvetkov Notebook <asessor-ts@yandex.ru>
2024-05-26 15:05:16 +0300

    добавлена функция отрисовки карточек сайта

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list')
+
+function deleteCard(evt) {
+    const cardItem = evt.target.closest('.card');
+    cardItem.remove();
+};
+
+function createCard (dataCard, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener('click', deleteCard);
+    
+    cardItem.querySelector('.card__image').src = dataCard.link;
+    cardItem.querySelector('.card__title').textContent = dataCard.name;
+
+    return cardItem;
+}
+
+function createContent(dataCards, deleteCard) {
+    dataCards.forEach(element => {
+        placesList.append(createCard(element, deleteCard));
+    });
+};
+
+createContent(initialCards, deleteCard)
